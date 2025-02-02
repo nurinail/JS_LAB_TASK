170c7d266237162d469aedb5999b8c2704355411
Nail Nuri <nurinail2003@gmail.com>
2025-02-02 21:51:29 +0400

    Task 3 Completed - Simple TrendYol e-commerce

---
+++ TASK_3_AddToCard_TrendYol/js/products.js
@@ -0,0 +1,50 @@
+export const productsArr=[
+    {
+        id:1,
+        image:"images/products0.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:40,
+    },
+    {
+        id:2,
+        image:"images/products1.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:50,
+    },
+    {
+        id:3,
+        image:"images/products2.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:20,
+    },
+    {
+        id:4,
+        image:"images/products3.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:70,
+    },
+    {
+        id:5,
+        image:"images/products4.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:23,
+    },
+    {
+        id:6,
+        image:"images/products5.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:45,
+    },
+    {
+        id:7,
+        image:"images/products6.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:53,
+    },
+    {
+        id:8,
+        image:"images/products7.jpeg",
+        title:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat.",
+        price:62,
+    },
+]
---
+++ TASK_3_AddToCard_TrendYol/main.js
@@ -0,0 +1,56 @@
+import { productsArr } from "./js/products.js";
+import { addProducts } from "./js/addProducts.js";
+
+let productsSection = document.querySelector(".main_products");
+productsArr.map(
+  (e) =>
+    (productsSection.innerHTML += `
+    <div class="main_products_item" data-product-id="${e.id}">
+            <img
+              class="main_products_item_img"
+              src="${e.image}"
+              alt="photo"
+            />
+            <div class="main_products_item_bottom">
+              <p class="main_products_item_bottom_name">${e.title}</p>
+              <h2 class="main_products_item_bottom_price">
+                Cəmi ${e.price}$
+                <a class="main_products_item_bottom_price_shopBtn" href="#"
+                  ><span class="material-symbols-outlined">
+                    shopping_cart
+                  </span></a
+                >
+              </h2>
+            </div>
+            <button class="main_products_item_heartIcon" ><i class="fa-regular fa-heart"></i></button>
+          </div>
+    `)
+);
+
+let productItem = document.querySelectorAll(".main_products_item");
+productItem.forEach((e) => {
+  let shopAccount = document.querySelector(".header_nav_menu_shops_number");
+  let dataId=Number(e.getAttribute("data-product-id"));
+  let shopBtn = e.children[1].children[1].children[0];
+  let shopNumber = 0;
+  let isExist=addProducts.find(event=>event.id===dataId);
+  shopBtn.addEventListener("click", (even) => {
+    even.preventDefault();
+    shopNumber += 1;
+    shopAccount.innerHTML = `${shopNumber}`;
+    
+  });
+
+  let favoriteBtn = e.children[2];
+  favoriteBtn.addEventListener("click", (even) => {
+    even.preventDefault();
+    favoriteBtn.children[0].classList.toggle("fa-solid");
+    if(favoriteBtn.children[0].className.includes("fa-solid")){
+      addProducts.push(e);
+      console.log(addProducts)
+    }else{
+      addProducts.pop();
+      console.log(addProducts)
+    }
+  });
+});
