63b875ac7dc7029cfb03cc8fa5c8cd4f80d741b4
Nail Nuri <nurinail2003@gmail.com>
2025-02-13 22:52:46 +0400

    Task_4 completed

--- General_Repeat _Task/Task_4/main.js
+++ General_Repeat _Task/Task_4/main.js
@@ -1,127 +1,51 @@
-let toDoListes = document.querySelector(".add_section_lista_ul");
+let toDoListesUl = document.querySelector(".add_section_lista_ul");
 let form = document.querySelector(".add_section_form");
 let headerDate=document.querySelector(".header_left_date")
 let accountList=document.querySelector(".container_header_rightCircle_span")
-let listMessageArr = [];
-// let listItemData=[];
-let date=new Date();
+let listMessageArr = []; 
+let date=new Date(); 
 headerDate.innerHTML=`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
 
-function refreshScreen() {
-  toDoListes.innerHTML = listMessageArr
-    .map(
-      (list, index) =>
-        `
+function getWrite(){
+  toDoListesUl.innerHTML=listMessageArr.map((msg,index)=>(
+    `
     <li class="add_section_lista_ul_item" data-mainListId="${index}">
-      <input class="add_section_lisl_item_text" type="text" value="${list}" readonly><button class="add_section_lista_ul_item_button"><i class="fa-solid fa-bars"></i></button>
-      <div class="add_section_lista_ul_item_popap activee">
-        <button class="add_section_lista_ul_item_popap_btn remove" data-listId="${index}">Remove</button>
-        <button class="add_section_lista_ul_item_popap_btn change">Change</button>
+      <input class="add_section_lisl_item_text" type="text" value="${msg}" readonly>
+      <button class="add_section_lista_ul_item_button"><i class="fa-solid fa-bars"></i></button>
+      <div class="active">
+        <button class="remove add_section_lista_ul_item_popap_btn" data-listId="${index}">Remove</button>
+        <button class="change add_section_lista_ul_item_popap_btn">Change</button>
       </div>
     </li>
     `
-    )
-    .join("");
+  )).join("");
+  accountList.innerHTML=`${listMessageArr.length}`
 }
-function getAccountList(){
-  accountList.innerHTML = listMessageArr.length;
+getWrite();
+
+function getAdd(e){
+e.preventDefault()
+let formInput=document.querySelector(".add_section_form_input");
+listMessageArr.push(formInput.value);
+getWrite();
+formInput.value="";
 }
-function activeToggle() {
-  document.addEventListener("click", (event) => {
-    if (event.target.closest(".add_section_lista_ul_item_button")) {
-      let list = event.target.closest(".add_section_lista_ul_item").querySelector(".add_section_lista_ul_item_popap");
-      list.classList.toggle("activee");
-    }
-  });
-};
-function removeItem(event) {
-  if (event.target.classList.contains("remove")) {
-    let removeId = event.target.getAttribute("data-listId");
-    listMessageArr.splice(removeId, 1);
-    getAccountList();
-    refreshScreen();
+toDoListesUl.addEventListener("click",(e)=>{
+  if(e.target.classList.contains("remove")){
+    let removeId=Number(e.target.getAttribute("data-listId"));
+    listMessageArr.splice(removeId,1);
+    getWrite();
   }
-};
-function changeItem(ev){
-let changeBtn=document.querySelectorAll(".change");
-changeBtn.forEach(change=>{
-  change.addEventListener("click",(e)=>{
-   let itemInput=e.target.parentElement.parentElement.children[0];
-   itemInput.toggleAttribute("readonly");
-  })
-  
+  if(e.target.classList.contains("change")){
+    let listText=e.target.parentElement.previousElementSibling.previousElementSibling;
+    listText.toggleAttribute("readonly");
+  }
+  if(e.target.classList.contains("fa-bars")){
+    let toggleItem=e.target.parentElement.nextElementSibling;
+    toggleItem.classList.toggle("active");
+    toggleItem.classList.toggle("add_section_lista_ul_item_popap");
+
+  }
 })
 
-}
-function sendData(e) {
-  e.preventDefault();
-  let inputText = document.querySelector(".add_section_form_input");
-  if (inputText.value.trim() !== "") {
-  listMessageArr.push(inputText.value);
-    getAccountList();
-    refreshScreen();
-  }
-  inputText.value = "";
-};
-toDoListes.addEventListener("click", removeItem);
-toDoListes.addEventListener("click", changeItem);
-form.addEventListener("submit", sendData);
-activeToggle();
-
-
-
-
-
-
-
-
-
-
-
-// function activeToggle(){
-//   let toggleBtn=document.querySelectorAll(".add_section_lista_ul_item_button");
-//   toggleBtn.forEach(e=>{
-//     e.addEventListener("click",()=>{
-//       let list=e.nextElementSibling;
-//       list.classList.toggle("activee")
-//     })
-//   });
-// }
-// function remove(){
-//   removeBtn=document.querySelectorAll(".remove");
-//   removeBtn.forEach(remove=>{
-//     remove.addEventListener("click",()=>{
-//      let removeId=remove.getAttribute("data-listId");
-//      let filterList=listItemData.filter(list=>list.getAttribute("data-mainListId")!=removeId);
-//      let newListMessageArr=[];
-//      filterList.map(item=>(
-//       newListMessageArr.push(item.children[0].innerHTML)
-//      ));
-//      listMessageArr=newListMessageArr;
-//      refreshScreen();
-  
-//     })
-//   })
-
-// }
-
-// function getListData(){
-//   let listItem=document.querySelectorAll(".add_section_lista_ul_item");
-//   listItemData.push(listItem[listItem.length-1]);
-
-// }
-
-
-// function sendData(e) {
-//   e.preventDefault();
-//   let inputText = document.querySelector(".add_section_form_input");
-//   listMessageArr.push(inputText.value);
-//   accountList.innerHTML=listMessageArr.length
-//   refreshScreen();
-//   getListData();
-//   activeToggle();
-//   remove();
-//   console.log(listMessageArr)
-// }
-
-// form.addEventListener("submit", sendData);
+form.addEventListener("submit",getAdd);
